0676589d6500cdf79326222ccbc8d98ace99951b
tan25gonz-ux <tan25gonz@gmail.com>
2025-11-21 18:15:03 -0600

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,20 +1,16 @@
-import { auth } from "./firebase.js";
+import { auth } from "../firebase.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 
 const form = document.getElementById("formLogin");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
 
   try {
     await signInWithEmailAndPassword(auth, email, password);
-
-    // 🔥 Redirección al dashboard del admin
     window.location.href = "admin.html";
-
   } catch (error) {
     alert("Correo o contraseña incorrectos.");
     console.error(error);
